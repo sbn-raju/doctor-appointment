459cc0e51798c329e5b37413ea17da23a4ffd4c1
Rakesh Penugonda <119618769+Rakesh-116@users.noreply.github.com>
2024-05-14 01:07:28 +0530

    add changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
     <>
       <h1 className='bg-orange-400'>JAI SHRI RAM</h1>
+      <h1 className='bg-blue-400'>Hello World!</h1>
     </>
   )
 }
