e63dc7eee082b303431316dcc4777d623ecc8e7e
sbn raju <simhardrinagaraju@gmail.com>
2024-05-14 01:33:20 +0530

    Added App changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,9 @@
-import { useState } from 'react'
 import Footer from './components/Footer/Footer'
-import HeaderHome from './components/Header/HeaderHome'
 
 
 //EVERY ONE USE APP FOR TESTING YOUR COMPONENT
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <h1 className='bg-orange-400'>JAI SHRI RAM</h1>
