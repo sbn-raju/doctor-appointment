4c618f7e691093f1a40b4e8a5bc6f6ee4bd73a0e
sbn raju <simhardrinagaraju@gmail.com>
2024-05-13 07:45:45 +0530

    Footer First section done

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -9,8 +9,27 @@ import { FaTelegram } from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <>
-    Footer
+    <> 
+      <div className="max-w-full h-auto flex flex-col bg-green-900 md:flex-row">
+        <div className="max-w-full h-60 flex justify-center items-center flex-col sm:w-1/3 sm:h-96">
+            <div className="w-full h-1/4 pl-4 sm:text-center">
+                  <h2 className="text-white font-semibold">LET'S WORK TOGETHER</h2>
+                  <h3 className="text-white font-semibold">For diseases free world</h3>
+            </div>
+            <div className="w-full h-1/2 pl-4 flex flex-row sm:flex-col md:flex-col">
+              <div className="flex justify-center items-center">
+                <img src={logo} alt="Logo Image" />
+              </div>
+              <div className="flex justify-center items-center pl-2">
+                <p className="text-white font-light sm:text-center sm:w-52">Dr.Padma &amp; Dr.Ramachandra Naturopathy</p>
+              </div>
+            </div>
+        </div>
+        <div>
+          
+        </div>
+        <div>box3</div>
+      </div>
     </>
   );
 };
